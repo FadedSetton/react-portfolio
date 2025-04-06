0eb31a7ab7379e31c1be0dfcb995975bc8e18f22
Christopher Munoz <chrismunozpro@gmail.com>
2025-04-06 14:32:50 -0700

    added aboutme info and profile img

--- react-portfolio/src/pages/About.jsx
+++ react-portfolio/src/pages/About.jsx
@@ -1,5 +1,22 @@
 function About() {
-    return <section><h2>About Me</h2><p>This is the About Me page.</p></section>;
+    return (
+      <section className="about">
+        <img
+          src="./src/assets/profileImg.png"
+          alt="Profile"
+          className="profile-pic"
+        />
+        <div className="bio">
+          <h2>About Me</h2>
+          <p>
+            Hi my name is Christopher Munoz. I am a student currently enrolled in a Full Stack Development Bootcamp at UO Eugene Oregon. I have a passion for tech and I am always looking to learn new things. I have experience with HTML, CSS, JavaScript, React, Node.js, Express.js, and MongoDB. I am currently looking for a job in the tech industry where I can apply my skills and continue to grow as a developer.
+          </p>
+          <p>
+            In my free time, I enjoy listening to music, playing video games (Destiny, Monster Hunter, Soul-Like, etc.. ), going to the gym, and delving into entertainment with complex/deep lore.
+          </p>
+        </div>
+      </section>
+    );
   }
   
   export default About;
